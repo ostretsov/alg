825ef6a01ae96995370a30039557ddff3a777a82
Artem Ostretsov <OstretsovAA@gmail.com>
2018-12-28 09:42:12 +0300

    Test multiply with long numbers

--- ch1/grade_school_multiplication_test.go
+++ ch1/grade_school_multiplication_test.go
@@ -2,14 +2,43 @@ package ch1
 
 import "testing"
 
-func TestMultiplication(t *testing.T) {
-	a := "5678"
-	b := "1234"
+func TestGradeSchoolMultiply(t *testing.T) {
+	t.Run("invalid arguments", func(t *testing.T) {
+		a := "5678.1"
+		b := "1234"
 
-	want := "7006652"
-	got, _ := GradeSchoolMultiply(a, b)
+		_, err := GradeSchoolMultiply(a, b)
 
-	if got != want {
-		t.Errorf("want %s got %s", want, got)
-	}
+		if err == nil {
+			t.Errorf("expected error on invalid argument")
+		}
+	})
+
+	t.Run("short numbers of same length", func(t *testing.T) {
+		a := "5678"
+		b := "1234"
+
+		want := "7006652"
+		got, _ := GradeSchoolMultiply(a, b)
+
+		if got != want {
+			t.Errorf("want %s got %s", want, got)
+		}
+	})
+
+	t.Run("long numbers of same length", func(t *testing.T) {
+		a := "5678293874908729034090283478162094598729348394875987123987234598701083548734909038857698911"
+		b := "1234489750984985098037129384738479193757692173718457766635242173950696712902048171635493875"
+
+		want := "7009795591655643027172461295023953881167542446640054000765770377106022505948006729526866614890982359431931298734376531996538221533914172897553259381603807010934556812446435534670125"
+		got, err := GradeSchoolMultiply(a, b)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if got != want {
+			t.Errorf("want %s got %s", want, got)
+		}
+	})
 }
